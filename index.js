632896a9d67fc204a8977428c411048c02d0e515
Nijanshu <46223306+Nijanshu@users.noreply.github.com>
2023-11-18 11:08:30 +0530

    done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // where your node app starts
 
 // init project
-require('dotenv').config();
+// require('dotenv').config();
 var express = require('express');
 var app = express();
 
@@ -19,6 +19,13 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/api/whoami',(req,res) =>{
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+  });
+})
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
